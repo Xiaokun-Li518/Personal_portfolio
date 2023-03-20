7f4f8a9fd880125c9a27f2f018b46ab85ce99cbf
li,xiaokun <leeli@iu.edu>
2023-03-20 15:43:05 -0400

    move download resume into intro

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import Brightness2Icon from '@mui/icons-material/Brightness2';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+
+import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
 
 const DarkMode = () => {
     const [theme, setTheme] = useState(null);
@@ -27,12 +29,12 @@ const DarkMode = () => {
   }, [theme]);
 
   return (
-    <div>
+    <div className="flex flex-row justify-between">
       <button type="button" onClick={handleThemeSwitch} className="p-5">
         {theme === "dark" ? (
-          <WbSunnyIcon sx={{ "&:hover": { color: "gray" } }} />
+          <WbSunnyIcon sx={{ fontSize:{xs:25, md:35}, "&:hover": { color: "gray" } }} />
         ) : (
-          <Brightness2Icon sx={{ "&:hover": { color: "gray" } }} />
+          <Brightness2Icon sx={{ fontSize:{xs:25, md:35}, "&:hover": { color: "gray" } }} />
         )}
       </button>
     </div>
