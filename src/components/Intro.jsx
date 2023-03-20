2bb68d9f4d0bb1f7556879dc00d55dee5451c645
li,xiaokun <leeli@iu.edu>
2023-03-20 15:08:09 -0400

    adjust mb for intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,8 +8,8 @@ const Intro = () => {
   return (
     <div className="flex items-center justify-center flex-col text-center md:text-start pt-20 pb-6">
         <h1 className="text-4xl md:text-5xl mb-1 md:mb-3 font-bold">Xiaokun Li</h1>
-        <h3 className="text-sm md:text-xl mb-6 font-normal">Software Engineer Intern</h3> 
-        <a className="text-sm md:text-lg mb-6 font-normal hover:font-semibold" href="/assets/xiaokunli.pdf" download>Download Resume <DownloadForOfflineIcon sx={{ fontSize:{xs:25, md:30}, "&:hover": { color: "gray" } }}/></a>
+        <h3 className="text-sm md:text-xl mb-3 md:mb-6 font-normal">Software Engineer Intern</h3> 
+        <a className="text-xs md:text-lg mb-3 md:mb-6 font-normal hover:font-semibold" href="/assets/xiaokunli.pdf" download>Download Resume <DownloadForOfflineIcon sx={{ fontSize:{xs:25, md:30}, "&:hover": { color: "gray" } }}/></a>
         <p className="test-sm max-w-xl mb-6 font-semibold"> I am a dedicated and hardworking computer science student who is currently on the hunt for an internship opportunity. I am in third year of my computer science program with a minor in mathematics and have always been passionate about technology and its applications. As I approache graduation, I am keen to gain industry experience through an internship program. I am looking for an opportunity to apply my skills and knowledge in a real-world setting, work alongside experienced professionals, and contribute to meaningful projects.  <br />(To be Graduated in May 2024).</p>
         <br />
         <div className="flex flex-row items-center align-middle gap-3 mx-auto">
