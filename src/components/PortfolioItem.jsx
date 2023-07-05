a6990ff46bbeed2b0dd98db349651d8114a0cad8
li,xiaokun <leeli@iu.edu>
2023-07-04 22:28:14 -0400

    add effect when hovering

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -4,7 +4,11 @@ import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 
 const PortfolioItem = ({title, imgUrl, stack, link}) => {
   return (
-    <Card sx={{width: {xs:'320px', sm:'320px', md: '358px'}, height:{xs:'336px', sm:'336px', md:'336px'}, borderRadius: '15px', boxShadow: 'none'}} className="border-2 border-stone-900 dark:bg-stone-900 dark:text-white dark:border-stone-100 dark:hover:border-gray-600 hover:border-stone-300 rounded-xl overflow-hidden">
+    <Card sx={{width: {xs:'320px', sm:'320px', md: '358px'}, height:{xs:'336px', sm:'336px', md:'336px'}, borderRadius: '15px', boxShadow: 'none', 			transition: 'all 0.3s ease', // Added line for smooth transition
+    '&:hover': {  // Added lines for hover effect
+      transform: 'scale(1.03)',
+      boxShadow: '5px 5px 15px rgba(0,0,0,0.3)',
+    },}} className="border-2 border-stone-900 dark:bg-stone-900 dark:text-white dark:border-stone-100 dark:hover:border-gray-600 hover:border-stone-300 rounded-xl overflow-hidden">
       <a href={link} target="_blank">
         <div> 
           <CardMedia image={imgUrl}
